c410c4fb9a191467963aa85ca3f93c67942a18a5
Michael Rubinstein <michael@polyblog.com>
2016-09-09 18:17:58 -0700

    hw_08 fully completed

--- michael-rubinstein/hw_08/4_templating_lab/main.js
+++ michael-rubinstein/hw_08/4_templating_lab/main.js
@@ -4,3 +4,35 @@
  * Specify your JavaScript app using an object rather than a set of functions.
  */
 
+var App = {};
+
+App.cookbook = {
+    "recipes" : [
+        {
+            "name" : "Shakshuka",
+            "serves" : 4,
+            "meal" : "breakfast"
+        },
+        {
+            "name" : "Black Bean Chili",
+            "serves" : 8,
+            "meal" : "dinner"
+        },
+        {
+            "name" : "Classic Peanut Butter Cookies",
+            "serves" : 36,
+            "meal" : "dessert"
+        }
+    ]
+};
+
+App.build = function() {
+    var source = $("#cookbookTemplate").html();
+    var templateProcessor = Handlebars.compile(source);
+    var output = templateProcessor(App.cookbook);
+    return output;
+}
+
+$("document").ready(function() {
+  $("body").append(App.build);
+});
