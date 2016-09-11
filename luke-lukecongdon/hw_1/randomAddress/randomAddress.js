22ef672fda9a589937a343fc016821da65a416b0
luke congdon <lukecongdon@gmail.com>
2016-09-11 12:25:00 -0700

    adding random address generator hw

--- luke-lukecongdon/hw_1/randomAddress/randomAddress.js
+++ luke-lukecongdon/hw_1/randomAddress/randomAddress.js
@@ -1,20 +1,3 @@
-// notes from fizzbuzz below for reference 
-/*
-
-for(var i = 1; i <= 100; i++) {
-  //  if ((i % 3) && (i % 5) === 0){    // funny but didn't work
-    if (((i % 3) === 0) && ((i % 5) === 0)){
-        console.log("FizzBuzz");
-    } else if ((i % 3) === 0){
-        console.log("Fizz");
-    } else if ((i % 5) === 0){
-        console.log("Buzz");
-    }
-    else {console.log(i);}
-}
-
-*/
-
 // Assignment 2: Random Address Generator
 
 // Write a script that can generate random addresses
@@ -24,11 +7,81 @@ for(var i = 1; i <= 100; i++) {
 // node random-address.js
 // => 34578 Dolphin Street, Wonka NY, 44506
 
+// start with some arrays below
+
+var stNum = [
+    "23",
+    "33",
+    "57",
+    "103",
+    "883"
+];
+var stName = [
+    "Mercy Street",
+    "Whipple Avenue",
+    "Van Ness Avenue",
+    "Great America",
+    "Pennsylvania Avenue"
+];
+var cityName = [
+    "Washington",
+    "San Francisco",
+    "Sunnyvale",
+    "San Mateo",
+    "Napa"
+];
+var stateName = [
+    "CA",
+    "WA",
+    "OR",
+    "AZ",
+    "CO"
+];
+var zipCode = [
+    "94110",
+    "94111",
+    "94112",
+    "94113",
+    "94114"
+];
 
 
+// idea, create random numbers for array elements below
+
+/*
+var randStNum = Math.floor(Math.random() * 5) + 1;
+    // console.log(randStNum);   // test, yes, getting numbers between 1-5
+var randStName = Math.floor(Math.random() * 5) + 1;
+var randCityName = Math.floor(Math.random() * 5) + 1;
+var randStateName = Math.floor(Math.random() * 5) + 1;
+var randZipCode = Math.floor(Math.random() * 5) + 1;
+
+*/
+// procedure above. choose random number between 0-1, multiply it times 5 to get number between 0 and 'less than 5', then use Math.floor to chop off decimals without rounding up. this gets whole numbers between 0-4 randomly. Last add 1 to all results to get random whole numbers between 1-5. 
+// referred to Rolling Dice example
+
+// NEXT, can I take a random 1-5 number and apply it to an array position to randomly select addresses, street numbers, etc? 
 
 
+// lost
+// checking Google, this seems simple, need to do this for each part of the street address 
+// e.g.    var item = items[Math.floor(Math.random()*items.length)];
+// my arrays are stNum, stName, cityName, stateName, zipCode
+// so...
+
+var number = stNum[Math.floor(Math.random()*stNum.length)];
+//    console.log(number);
+// array name function and length must match 
+var name = stName[Math.floor(Math.random()*stName.length)];
+//    console.log(name);
+var city = cityName[Math.floor(Math.random()*cityName.length)];
+//    console.log(city);
+var state = stateName[Math.floor(Math.random()*stateName.length)];
+//    console.log(state);
+var zip = zipCode[Math.floor(Math.random()*zipCode.length)];
+//    console.log(zip);
+
+console.log(number + " " + name + ", " + city + " " + state + ", " + zip);
 
 
-
-
+// FINAL FOR NOW: This works in browser console, but not in node CLI mode
