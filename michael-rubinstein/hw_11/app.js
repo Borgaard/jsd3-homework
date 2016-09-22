980b9e6822fbcfcc159b3f99750b4565928f2718
Michael N. Rubinstein <michael@polyblog.com>
2016-09-21 18:54:33 -0700

    Completed bonus - now shows user info

--- michael-rubinstein/hw_11/app.js
+++ michael-rubinstein/hw_11/app.js
@@ -1,4 +1,5 @@
 $(function() {
+  // var firstName = "Somebody";
   // DOM is now ready
   _500px.init({
     sdk_key: 'c095a1ed8923d151066ec81a6569a9dd1195e5c5'
@@ -12,17 +13,25 @@ $(function() {
   // Event handler for successful login
   _500px.on('authorization_obtained', function() {
     $('.sign-in-view').hide();
-    $('.image-results-view > h3').before('<h3>Greatings, Melvin!</h3>');
-    $('.image-results-view').show();
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
+        getUser();
         getPhotos(position);
+        $('.image-results-view').show();
       });
     } else {
       $('.images').append("Sorry, your pathetic browser doth not geolocate!");
     }
   });
+
+  // Get current user info
+  function getUser() {
+    _500px.api('/users', function (response) {
+      firstName = response.data.user.firstname;
+      $('.image-results-view > h3').before(`<h3>Greatings, ${firstName}!</h3>`);
+    });
+  }
 
   // Create query for photos and send it to 500px
   function getPhotos(position) {
